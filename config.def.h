3b33b6471964023847ef244fdc8d23fb37d8092a
tareefarz <tareef_arz@outlook.com>
2023-04-15 16:44:01 +0600

    Another Update

--- config.def.h
+++ config.def.h
@@ -15,7 +15,7 @@ static const char dmenufont[]       = "CascadiaCode:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#FFFFFF";
 static const char col_cyan[]        = "#492080";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
